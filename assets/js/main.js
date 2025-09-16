f95ae5b1db6f63e0199db0bd09819accf355d18d
RasmusLeddyr <rasmusleddyr@gmail.com>
2025-09-16 09:35:10 +0200

    fetch push

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// import and run component functions
+
+/*  Eksempel:
+import { Gallery } from "./components/gallery.js";
+Gallery();
+
+import { Header } from "./components/header-container.js";
+Header();
+ */
+
+//
