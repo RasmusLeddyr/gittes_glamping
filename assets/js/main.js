dab118ac7b5e12a6fccfabaa03bd8178f4817d75
Mathias Boll <math50v7@mcdmstudent.dk>
2025-09-17 10:17:52 +0200

    topbar og slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
+import { initTopbar } from './components/topbar.js';
+import { initSlider } from './components/slider.js';
 import { initFooter } from './components/footer.js';
 
 console.log('[main.js] loaded');
+
+initTopbar();
+initSlider();
 initFooter();
